79f709b667d119e67b61e4b387be241c932d95bd
Finn312 <finnah312@gmail.com>
2026-04-24 23:43:37 +0200

    fix cors

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from slots import spin_reels, calculate_win as slots_calculate_win
 from dice import calculate_win as dice_calculate_win
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
